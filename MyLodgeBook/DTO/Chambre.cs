47b6da3a6c9d78865b789ba912d2c02d71589112
chiwawaweb <git@chiwawaweb.com>
2018-01-16 14:09:49 +0100

    Maj

---
+++ MyLodgeBook/DTO/Chambre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLodgeBook.DTO
+{
+    public class Chambre
+    {
+        public int? Id { get; set; }
+        public string Numero { get; set; }
+        public string Nom { get; set; }
+        public int NombreLits { get; set; }
+        public bool Fumeur { get; set; }
+        public bool Animaux { get; set; }
+    }
+}
